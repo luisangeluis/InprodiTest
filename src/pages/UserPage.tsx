e0b65f26180db6f8b7ddf1fbba0e7f9158178099
luisangeluis <luisangeluis@gmail.com>
2024-01-02 15:45:21 -0600

    Added styles to user page

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -14,39 +14,48 @@ const UserPage=()=>{
   const user = users.find((user)=>user.id===Number(id))
   const { data, error, isLoading } = useGetUserWeatherQuery(user);
   console.log(data);
+  const getData =()=>{
+    if(isLoading) return <h3>Loading weather...</h3>
+
+    if(error) {
+      if("message" in error) return <h3>{error.message}</h3>
+      else return <h3>An error occurred</h3>
+    }
+
+    if(data){
+      return(
+        <div className={styles.weatherInfo}>
+          <p>City: {data.timezone }</p>
+          <p>Temperature: {data.current.temp}</p>
+          <p>Humidity: {data.current.humidity}</p>
+          <p>Wind speed: {data.current.wind_speed}</p>
+          <p>Weather: {data.current.weather[0].main}</p>
+          <p>Description: {data.current.weather[0].description}</p>
+          <img src={`https://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`}></img>
+        </div>
+      )
+    }
+  }
+
   return(
     <section className={styles.userPage}>
-      <article className={styles.userInfo}>
-        <h2>User data</h2>
-        {
-          !user?.id ? 
-            <h2>No user</h2> :
-            (
-              <>
+      <h2>User weather</h2>
+      <article className={styles.userDetail}>
+        {!user?.id ? 
+          <h2>User doesn't exist</h2> :
+          (
+            <>
+              <div className={styles.userInfo}>
                 <h2>User with id: {user.id}</h2>
                 <h2>Name: {user.name}</h2>
-                            
-              </>
-            )
-        }
-        {
-          !data ? 
-            <p>No data</p> :
-            (
-              <>
-                <p>City: {data.timezone }</p>
-                <p>Temperature: {data.current.temp}</p>
-                <p>Humidity: {data.current.humidity}</p>
-                <p>Wind speed: {data.current.wind_speed}</p>
-                <p>Weather: {data.current.weather[0].main}</p>
-                <p>Description: {data.current.weather[0].description}</p>
-                <img src={`https://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`}></img>
-              </>
-            )
+              </div>
+              {getData()}           
+            </>
+          )
         }
       </article>
     </section>
-    )
+  )
 }
 
 export default UserPage;
