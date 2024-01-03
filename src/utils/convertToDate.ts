35695fe6edab1e7588ecacb28b79479a35dbf2ab
luisangeluis <luisangeluis@gmail.com>
2024-01-03 01:26:11 -0600

    Configurated method convertToDate

--- src/utils/convertToDate.ts
+++ src/utils/convertToDate.ts
@@ -1,10 +1,10 @@
-const dateOptions = {
+const dateOptions:Intl.DateTimeFormatOptions = {
   year: 'numeric',
   month: '2-digit',
   day: '2-digit'
 };
 
-const timeOptions = {
+const timeOptions:Intl.DateTimeFormatOptions = {
   hour: '2-digit',
   minute: '2-digit',
   second: '2-digit'
